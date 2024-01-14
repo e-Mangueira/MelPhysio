1db441a4cdceced30bddc4056398af8fe5192b72
e-Mangueira <nm.mangueira@ufma.br>
2024-01-14 17:06:10 -0300

    Step 2 - NativeBase initial setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Principal from './src/Principal';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Primeiro App MelPhysio</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Principal />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
